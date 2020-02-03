83d169d750c58a0d07bce94466f2327015d480e0
Curtis pachtinger <cpach96@gmail.com>
2020-02-03 15:33:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |holiday , supplies|
-    puts holiday supplies 
+  holiday_hash.each do |holiday, supplies|
+    puts #{holiday} #{supplies
   end
 end
 
