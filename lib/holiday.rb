b79d0524d945c67502fb6e7c2c5f85354d654512
Curtis pachtinger <cpach96@gmail.com>
2020-02-03 16:04:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, h|
     puts "#{season.to_s.capitalize}:"
     h.each do |holiday,array|
-      str = holiday.to_s
+      str = holiday.to_s.split("_")
+      
     
   end
 end
