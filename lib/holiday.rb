630f37234290c7c6aeb74367e16e8dd325feacaa
Curtis pachtinger <cpach96@gmail.com>
2020-02-03 15:30:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter
-  holiday_hash[:winter].each do |holiday, supply|
+  holiday_hash[:winter].collect do |holiday, supply|
     supply
   end
   
