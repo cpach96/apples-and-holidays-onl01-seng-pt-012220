eeae88aeb6d382976b329302974af95ae44a6e7b
Curtis pachtinger <cpach96@gmail.com>
2020-02-03 15:20:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winte
-  holiday_hash[:winter].values
+  holiday_hash[:winter].value
 
 end
 
